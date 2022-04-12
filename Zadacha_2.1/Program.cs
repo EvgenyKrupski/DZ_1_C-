fc000cd91597c8ec16b19544efa76ab6b2b4c597
EvgenyKrupski <Krupev87@yandex.ru>
2022-04-12 20:00:12 +0300

    Решение задачи 2.1

---
+++ Zadacha_2.1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Третье число: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+int numbermax = numberA;
+
+if (numberB > numberA)
+{
+    numbermax = numberB;
+}
+if (numberC > numberA)
+{
+    numbermax = numberC;
+}
+if (numberB > numberC)
+{
+    numbermax = numberB;
+}  
+Console.Write("max = " + numbermax);
