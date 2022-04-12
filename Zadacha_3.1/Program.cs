303bca8fc1951bdc167b7a67f14ddadcc1bbf647
EvgenyKrupski <Krupev87@yandex.ru>
2022-04-12 20:23:11 +0300

    Решение задачи 3.1

---
+++ Zadacha_3.1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введи число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+int numberB = numberA % 2;
+
+if (numberB == 0)
+{
+ Console.WriteLine("Да");   
+}
+else
+{
+    Console.WriteLine("Нет");
+}
