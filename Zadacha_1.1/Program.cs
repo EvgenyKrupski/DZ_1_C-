fdef9e1c181194dcc9a1c82123477bf94dfb101b
EvgenyKrupski <Krupev87@yandex.ru>
2022-04-12 18:48:13 +0300

    Решение задачи 1.1

---
+++ Zadacha_1.1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("вторе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA>numberB)
+{
+    Console.Write ("max = " + numberA);
+}
+else
+{
+    Console.Write ("max = " + numberB);
+}
